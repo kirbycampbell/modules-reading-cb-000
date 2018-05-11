f64b74389e76bdaf5bfb19d5899afd498541fcb1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-11 17:00:52 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,17 @@ require_relative './fancy_dance.rb'
 
 
 class Kid
+
+  include Dance
+  extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+
+
 end
